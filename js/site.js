6b08e948db13e5d5aa28987a9793a007f5dad6b3
cmurphy11 <cmurphy11@hawk.iit.edu>
2021-03-16 17:57:39 -0500

    css/js updates
    messing with js, updated css

--- js/site.js
+++ js/site.js
@@ -2,3 +2,17 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
   });
+
+  function validationEvent() {
+    var a = document.forms["form-one"]["first-name"].value;
+    var b = document.forms["form-one"]["last-name"].value;
+    var c = document.forms["form-one"]["email"].value;
+    var d = document.forms["form-one"]["phone"].value;
+    var e = document.forms["form-one"]["dob"].value;
+    if ((a == "") || (b == "") || (c == "") || (d == "") || (e == "")){
+      alert("All of the sections must be filled out.");
+    }
+    else{
+      alert("Sign up success! We can't wait to explore the stars with you!");
+    }
+  }
