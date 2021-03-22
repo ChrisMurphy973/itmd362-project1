baaf96c15822ca353bd085628dfb8e939dcf2bab
cmurphy11 <cmurphy11@hawk.iit.edu>
2021-03-22 17:05:27 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -8,7 +8,7 @@ jQuery(function($) {
     var b = document.forms["form-one"]["last-name"].value;
     var c = document.forms["form-one"]["email"].value;
     var d = document.forms["form-one"]["phone"].value;
-    var e = document.forms["form-one"]["dob"].value;
+    var e = document.forms["form-one"]["DoB"].value;
     if ((a == "") || (b == "") || (c == "") || (d == "") || (e == "")){
       alert("Make sure all sections are properly filled out.");
     }
